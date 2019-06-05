Fix memory overflow issue with sklearn GridSearch

estimators/BayesianNFEstimator.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
-
-tfd = tfp.distributions
 from estimators.DistributionLayers import InverseNormalizingFlowLayer, MeanFieldLayer
 from estimators.BaseNFEstimator import BaseNFEstimator
+
+tfd = tfp.distributions
 
 
 class BayesianNFEstimator(BaseNFEstimator):
@@ -72,6 +72,8 @@ class BayesianNFEstimator(BaseNFEstimator):
         learning_rate=2e-2,
         trainable_prior=False,
     ):
+        # this is necessary, else there'll be processes hanging around hogging memory
+        tf.keras.backend.clear_session()
         return BayesianNFEstimator(
             n_dims,
             kl_weight_scale,
@@ -125,7 +127,7 @@ class BayesianNFEstimator(BaseNFEstimator):
         x_noise_std=0.0,
         activation="relu",
     ):
-        assert type(hidden_sizes) == tuple
+        assert type(hidden_sizes) == tuple or type(hidden_sizes) == list
         assert kl_weight_scale <= 1.0
         assert x_noise_std >= 0.0
 

estimators/MaximumLikelihoodNFEstimator.py
@@ -48,6 +48,8 @@ class MaximumLikelihoodNFEstimator(BaseNFEstimator):
         learning_rate=3e-3,
         activation="tanh",
     ):
+        # this is necessary, else there'll be processes hanging around hogging memory
+        tf.keras.backend.clear_session()
         return MaximumLikelihoodNFEstimator(
             n_dims=n_dims,
             flow_types=flow_types,
@@ -60,7 +62,7 @@ class MaximumLikelihoodNFEstimator(BaseNFEstimator):
         )
 
     def _get_dense_layers(self, hidden_sizes, output_size, x_noise_std, activation):
-        assert type(hidden_sizes) == tuple
+        assert type(hidden_sizes) == tuple or type(hidden_sizes) == list
         assert x_noise_std >= 0.0
 
         # these values are assigned once fit is called

evaluation/config_runner.py
@@ -28,16 +28,19 @@ def run_configuation(
             model = tf.keras.wrappers.scikit_learn.KerasRegressor(
                 build_fn=estimator["build_fn"], epochs=n_epochs, verbose=0
             )
+
             cv = GridSearchCV(
-                model,
+                estimator=model,
                 param_grid=estimator["param_grid"],
-                cv=n_folds,
                 scoring=estimator["scoring_fn"],
                 n_jobs=-1,
+                pre_dispatch="1*n_jobs",
                 iid=True,
-                verbose=2,
+                cv=n_folds,
                 refit=False,
+                verbose=10,
                 error_score=np.NaN,
+                return_train_score=False,
             )
 
             x_train, y_train = DENSITY_CLASSES[density_name](**density_params).simulate(
@@ -49,22 +52,33 @@ def run_configuation(
                 y_train = np.expand_dims(y_train, axis=1)
 
             cv.fit(x_train, y_train)
-            pandas.DataFrame(cv.cv_results_).to_csv(
+            df = pandas.DataFrame(cv.cv_results_)
+            print(df)
+            df.to_csv(
                 os.path.join(
                     results_dir,
-                    "results_{}_{}.csv".format(estimator["estimator_name"], density_name),
+                    "results_{}_{}.csv".format(
+                        estimator["estimator_name"], density_name
+                    ),
                 )
             )
 
-        # df = pandas.DataFrame()
-        # for density, _ in DENSITIES:
-        #     temp = pandas.read_csv(
-        #         os.path.join(
-        #             DATA_DIR,
-        #             "results_{}_{}.csv".format(estimator["estimator_name"], density.__name__),
-        #         )
-        #     )
-        #     temp = temp.join(pandas.Series([density.__name__] * len(temp), name="density"))
-        #     df = df.append(temp)
-        #
-        # df.to_csv(os.path.join(DATA_DIR, "results_{}.csv".format(estimator["estimator_name"])))
+        df = pandas.DataFrame()
+        for density, _ in density_list:
+            temp = pandas.read_csv(
+                os.path.join(
+                    results_dir,
+                    "results_{}_{}.csv".format(estimator["estimator_name"], density),
+                )
+            )
+            temp = temp.join(pandas.Series([density] * len(temp), name="density"))
+            temp = temp.join(
+                pandas.Series(estimator["estimator_name"] * len(temp), name="estimator")
+            )
+            df = df.append(temp)
+
+        df.to_csv(
+            os.path.join(
+                results_dir, "results_{}.csv".format(estimator["estimator_name"])
+            )
+        )

tests/test_evaluation.py
@@ -1,6 +1,11 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
 import numpy as np
 from estimators import BayesianNFEstimator, MaximumLikelihoodNFEstimator
 from evaluation.scorers import bayesian_log_likelihood_score, mle_log_likelihood_score
+import pytest
+
+tf.random.set_random_seed(22)
 
 
 class DummyWrapper:
@@ -9,26 +14,31 @@ class DummyWrapper:
 
 
 def test_bayesian_score():
-    x_train = np.linspace(-1, 1, 10).reshape((10, 1))
-    y_train = np.linspace(-1, 1, 10).reshape((10, 1))
+    # sinusoidal data with heteroscedastic noise
+    x_train = np.linspace(-3, 3, 300, dtype=np.float32).reshape((300, 1))
+    noise = tfp.distributions.MultivariateNormalDiag(
+        loc=5 * tf.math.sin(2 * x_train), scale_diag=abs(x_train)
+    )
+    y_train = noise.sample().numpy()
 
     mle = MaximumLikelihoodNFEstimator(
         1, flow_types=tuple(), hidden_sizes=(6, 6), trainable_base_dist=True
     )
-    mle.fit(x_train, y_train, epochs=10, verbose=0)
+    mle.fit(x_train, y_train, epochs=20, verbose=0)
     # deterministic, so should be the same
+    # mle furthermore has no regularisation loss / KL-divs added, therefore evaluate and nll are the same
     assert bayesian_log_likelihood_score(
         DummyWrapper(mle), x_train, y_train
-    ) == -mle.evaluate(x_train, y_train)
+    ) == pytest.approx(-mle.evaluate(x_train, y_train))
 
     be = BayesianNFEstimator(
         n_dims=1, flow_types=tuple(), hidden_sizes=(6, 6), trainable_base_dist=True
     )
-    be.fit(x_train, y_train, epochs=10, verbose=0)
-    # random, so shouldn't be the same
-    assert bayesian_log_likelihood_score(
-        DummyWrapper(be), x_train, y_train
-    ) != -be.evaluate(x_train, y_train)
+    be.fit(x_train, y_train, epochs=200, verbose=0)
+    score = bayesian_log_likelihood_score(DummyWrapper(be), x_train, y_train)
+    loss = be.evaluate(x_train, y_train)
+    # ass the loss has the KL div to the prior added to it, it's negative has to be smaller than the nll score
+    assert score > -loss
 
 
 def test_mle_score():
@@ -42,4 +52,4 @@ def test_mle_score():
     # deterministic, so should be the same
     assert mle_log_likelihood_score(
         DummyWrapper(mle), x_train, y_train
-    ) == -mle.evaluate(x_train, y_train)
+    ) == pytest.approx(-mle.evaluate(x_train, y_train))