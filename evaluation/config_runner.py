1e27a953adcd49f772ca4f6b18a9bd442ad485bf
Simon Boehm <simonboehm@mailbox.org>
2019-06-16 16:32:57 +0200

    Start and kill process for every CV run

--- evaluation/config_runner.py
+++ evaluation/config_runner.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import GridSearchCV, ShuffleSplit
 import pandas
 import tensorflow as tf
 from tensorflow.python import tf2
+from multiprocessing import Process
 from cde.density_simulation import SkewNormal, GaussianMixture, ArmaJump, EconDensity
 
 if not tf2.enabled():
@@ -12,14 +13,6 @@ if not tf2.enabled():
     tf.enable_v2_behavior()
     assert tf2.enabled()
 
-DENSITY_CLASSES = {
-    "SkewNormal": SkewNormal,
-    "ArmaJump": ArmaJump,
-    "EconDensity": EconDensity,
-    "GaussianMixture": GaussianMixture,
-}
-
-TEST_SIZE = 5 * 10 ** 5
 
 
 def run_configuation(
@@ -28,42 +21,63 @@ def run_configuation(
     for estimator in estimator_list:
         for density_name, density_params in density_list:
             for n_datapoints in n_datapoints_list:
-                x_train, y_train = DENSITY_CLASSES[density_name](**density_params).simulate(
-                    n_samples=n_datapoints + TEST_SIZE
+                p = Process(
+                    target=run_cv,
+                    args=(
+                        density_name,
+                        density_params,
+                        estimator,
+                        n_datapoints,
+                        n_epochs,
+                        n_folds,
+                        n_jobs,
+                        results_dir,
+                    ),
                 )
-                # Arma Jump has a dimension different from the others
-                if density_name == "ArmaJump":
-                    x_train = np.expand_dims(x_train, axis=1)
-                    y_train = np.expand_dims(y_train, axis=1)
+                p.start()
+                p.join()
 
-                estimator["param_grid"]["n_dims"] = [y_train.shape[1]]
-                if estimator["estimator_name"] == "bayesian":
-                    estimator["param_grid"]["kl_weight_scale"] = [0.5 / n_datapoints]
 
-                model = tf.keras.wrappers.scikit_learn.KerasRegressor(
-                    build_fn=estimator["build_fn"], epochs=n_epochs, verbose=0
-                )
+def run_cv(
+    density_name, density_params, estimator, n_datapoints, n_epochs, n_folds, n_jobs, results_dir
+):
+    density_classes = {
+        "SkewNormal": SkewNormal,
+        "ArmaJump": ArmaJump,
+        "EconDensity": EconDensity,
+        "GaussianMixture": GaussianMixture,
+    }
+    test_size = 5 * 10 ** 5
 
-                cv = GridSearchCV(
-                    estimator=model,
-                    param_grid=estimator["param_grid"],
-                    scoring=estimator["scoring_fn"],
-                    n_jobs=n_jobs,
-                    pre_dispatch="1*n_jobs",
-                    iid=True,
-                    cv=ShuffleSplit(n_splits=n_folds, train_size=n_datapoints, random_state=22),
-                    refit=False,
-                    verbose=10,
-                    error_score=np.NaN,
-                    return_train_score=False,
-                )
-
-                cv.fit(x_train, y_train)
-
-                df = save_single_result(
-                    cv.cv_results_, density_name, estimator, n_datapoints, results_dir
-                )
-                append_to_full_result(df, results_dir)
+    x_train, y_train = density_classes[density_name](**density_params).simulate(
+        n_samples=n_datapoints + test_size
+    )
+    # Arma Jump has a dimension different from the others
+    if density_name == "ArmaJump":
+        x_train = np.expand_dims(x_train, axis=1)
+        y_train = np.expand_dims(y_train, axis=1)
+    estimator["param_grid"]["n_dims"] = [y_train.shape[1]]
+    if estimator["estimator_name"] == "bayesian":
+        estimator["param_grid"]["kl_weight_scale"] = [0.5 / n_datapoints]
+    model = tf.keras.wrappers.scikit_learn.KerasRegressor(
+        build_fn=estimator["build_fn"], epochs=n_epochs, verbose=0
+    )
+    cv = GridSearchCV(
+        estimator=model,
+        param_grid=estimator["param_grid"],
+        scoring=estimator["scoring_fn"],
+        n_jobs=n_jobs,
+        pre_dispatch="1*n_jobs",
+        iid=True,
+        cv=ShuffleSplit(n_splits=n_folds, train_size=n_datapoints, random_state=22),
+        refit=False,
+        verbose=10,
+        error_score=np.NaN,
+        return_train_score=False,
+    )
+    cv.fit(x_train, y_train)
+    df = save_single_result(cv.cv_results_, density_name, estimator, n_datapoints, results_dir)
+    append_to_full_result(df, results_dir)
 
 
 def append_to_full_result(df, results_dir):
@@ -71,9 +85,8 @@ def append_to_full_result(df, results_dir):
     if not os.path.isfile(results_filename):
         df.to_csv(results_filename)
     else:
-        results_df = pandas.read_csv(results_filename)
-        results_df = results_df.append(df, ignore_index=True)
-        results_df.to_csv(results_filename)
+        results_df = pandas.read_csv(results_filename, index_col=0)
+        pandas.concat([results_df, df], sort=True, ignore_index=True).to_csv(results_filename)
 
 
 def save_single_result(result_dict, density_name, estimator, n_datapoints, results_dir):
