11fafd0f5eb8c2b85094da08d403e39a34fb4a2e
Simon Boehm <simonboehm@mailbox.org>
2019-06-25 20:45:01 +0200

    Update demo script

--- demo.py
+++ demo.py
@@ -1,52 +1,33 @@
+import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
-from tensorflow.python import tf2
-
-if not tf2.enabled():
-    import tensorflow.compat.v2 as tf
-
-    tf.enable_v2_behavior()
-    assert tf2.enabled()
-
-import numpy as np
 import matplotlib.pyplot as plt
-from simulation.dummy_data_gen import gen_cosine_noise_data
-from estimators import MaximumLikelihoodNFEstimator, BayesNormalizingFlowNetwork
-
+from simulation.dummy_data_gen import gen_cosine_noise_data, gen_trippe_hetero_data
+from estimators import *
+from cde.density_simulation import GaussianMixture, SkewNormal
+from evaluation.scorers import mle_log_likelihood_score, DummySklearWrapper
 from evaluation.visualization.flow_plotting import plot_model
 
 tfd = tfp.distributions
-tf.random.set_seed(22)
 np.random.seed(22)
 
-
-# x_train, y_train = gen_trippe_hetero_data(
-#   1, n_pts=400, heteroscedastic=True, bimodal=True
-# )
 x_train, y_train = gen_cosine_noise_data(300, noise_std=0.3, heterosced_noise=0.5)
+x_test, y_test = gen_cosine_noise_data(3000, noise_std=0.3, heterosced_noise=0.5)
 plt.scatter(x_train, y_train)
-# plt.show()
+plt.show()
 
 
-model = BayesNormalizingFlowNetwork(
+model = BayesKernelMixtureNetwork(
+    kl_weight_scale=0.3 / x_train.shape[0],
+    hidden_sizes=(7, 7),
     n_dims=1,
-    kl_weight_scale=1.0 / x_train.shape[0],
-    n_flows=5,
-    hidden_sizes=(10,),
-    trainable_base_dist=True,
+    n_centers=20,
     activation="tanh",
-    trainable_prior=False,
-    kl_use_exact=True,
-    map_mode=True,
+    learning_rate=1e-2,
+    map_mode=False,
 )
-# model = MaximumLikelihoodNFEstimator(
-#     n_dims=1,
-#     n_flows=5,
-#     hidden_sizes=(16, 16),
-#     trainable_base_dist=False,
-#     noise_reg=("rule_of_thumb", 0.1),
-# )
-model.fit(x_train, y_train, epochs=1000, verbose=2)
+model.fit(x_train, y_train, epochs=2000, verbose=2)
+print(mle_log_likelihood_score(DummySklearWrapper(model), x_test, y_test))
 
 x_swoop = np.linspace(-4, 4, num=100).reshape((100, 1))
 for _ in range(1):
